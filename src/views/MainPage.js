6acea9700facd75fcb1424441e43a2ed24c109de
Edward Childress <ebc1210@BaylorGenetics.com>
2020-11-07 13:27:29 -0600

    landing page working

--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -9,15 +9,17 @@ import {
   Button,
 } from "@triframe/designer";
 
-export const MainPage = tether(function* ({ Api }) {
+export const MainPage = tether(function* ({ Api, redirect }) {
   return (
     <Container>
       <Heading>PRECARIOUS!</Heading>
 
       <Subheading>Login or Create User</Subheading>
       <Section>
-        <Button onClick={() => <Redirect to="/login" />}>Log In</Button>
-        <Button onClick={() => <Redirect to="/create-user" />}>Register</Button>
+        <Button onClick={() => redirect("/login")}>Log In</Button>
+        <Button onClick={() => redirect("/create-user")}>
+          Register New User
+        </Button>
       </Section>
     </Container>
   );
