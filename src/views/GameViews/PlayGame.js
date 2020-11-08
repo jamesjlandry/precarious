c0a6c8078b38fc22cb91568cc9540be8ce90d98c
animog504 <animoogle504@gmail.com>
2020-11-08 10:42:14 -0600

    PlayGame landing page stable but still under construction

---
+++ src/views/GameViews/PlayGame.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Container,
+  Heading,
+  tether,
+  Section,
+  TextInput,
+  Subheading,
+  Button,
+  Area,
+  Card,
+} from "@triframe/designer";
+
+
+export const PlayGame = tether(function* ({ Api, redirect }) {
+  //const { Players } = Api;
+ 
+  const cardStyle = {padding: '10px', borderStyle:'solid', borderRadius:'15px', backgroundColor:"rgb(245,245,245)"}
+  const buttonStyle = {
+    active: {backgroundColor:"green", borderRadius:"15px"},
+    inactive: {backgroundColor:"rgb(230,230,230)", borderRadius:"15px"},
+  }
+  const currentUser = yield User.current();
+
+
+    /**
+     * create components
+     */
+
+    // const playerCard = {};
+  return (
+    <Container>
+        <Area alignX="right" style={{padding: '10px'}} >
+            <Card style={cardStyle}>
+            <Heading><b>Players</b></Heading>
+            
+            <Section>
+
+             player here
+            
+            </Section>
+            </Card>
+        </Area>
+    </Container>
+  );
+});
