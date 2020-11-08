39419a283d15d509496c64214871c92c76883a23
animog504 <animoogle504@gmail.com>
2020-11-08 13:55:52 -0600

    current stable. building out game screen

--- src/views/GameViews/PlayGame.js
+++ src/views/GameViews/PlayGame.js
@@ -13,34 +13,48 @@ import {
 
 
 export const PlayGame = tether(function* ({ Api, redirect }) {
-  //const { Players } = Api;
+    const api = Api;
+    console.log(api)
+  const {Player, Game} = Api;  
+             
+ 
  
   const cardStyle = {padding: '10px', borderStyle:'solid', borderRadius:'15px', backgroundColor:"rgb(245,245,245)"}
   const buttonStyle = {
     active: {backgroundColor:"green", borderRadius:"15px"},
     inactive: {backgroundColor:"rgb(230,230,230)", borderRadius:"15px"},
+    buzzer: {minHeight: "25vh", backgroundColor:"red", borderRadius:"50px"}
   }
   const currentUser = yield User.current();
+  const answering = false;
 
-
-    /**
-     * create components
-     */
-
-    // const playerCard = {};
   return (
     <Container>
+        <Area>
+        <Heading>round: {Game.currentRound ? Game.currentRound : '-'}/{Game.rounds ? Game.rounds : '-'} </Heading>
+        </Area>
         <Area alignX="right" style={{padding: '10px'}} >
             <Card style={cardStyle}>
             <Heading><b>Players</b></Heading>
-            
-            <Section>
+                <Section>{Game.players ? Game.players.map((player) => 
+                    <Card>
+                        {player.name} : {player.score}
+                    </Card>
+                ) : null}
 
-             player here
-            
-            </Section>
+                </Section>
             </Card>
         </Area>
+        <Area alignY="bottom">
+        <Card style={cardStyle}>
+            <Heading><b>Player Window</b></Heading>
+                <Button style={buttonStyle.buzzer} onClick={()=> {
+                    console.log("buzz in!")
+                    /*currentPlayer.buzzIn*/
+                    }}>Buzzer</Button>
+            </Card>
+        </Area>
+
     </Container>
   );
 });
